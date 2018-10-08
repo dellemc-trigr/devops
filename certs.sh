0ccc226d7ba37a8573229d30e4c63fdaa8bc925e
Xuebin He <xuebin.he@emc.com>
2018-10-08 13:57:48 -0400

    Combine domain and port

--- certs.sh
+++ certs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-domain=${1}
-port=${2:-443}
+address=${1}
 
-certs_content=`ex +'/BEGIN CERTIFICATE/,/END CERTIFICATE/p' <(echo | openssl s_client -showcerts -connect ${domain}:${port} < /dev/null 2>/dev/null) -scq`
-echo "${certs_content}" | sudo tee -a /usr/local/share/ca-certificates/${domain}.crt
+certs_content=`ex +'/BEGIN CERTIFICATE/,/END CERTIFICATE/p' <(echo | openssl s_client -showcerts -connect ${address} < /dev/null 2>/dev/null) -scq`
+echo "${certs_content}" | sudo tee -a /usr/local/share/ca-certificates/${address}.crt
 
 sudo update-ca-certificates
